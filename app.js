9984ffca7c771246ed6261bc2db2ca60bfd97740
emirakbarov <126677803+emirakbarov@users.noreply.github.com>
2023-11-15 22:48:21 +0000

    website create set up server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view-engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+})
+
+app.listen(3000);
