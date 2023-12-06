e852dd81f5ba2b50a4f5aa22feefae2b5c9f599e
Emir <Emir@MBP-of-Emir.cust.communityfibre.co.uk>
2023-12-06 19:39:36 +0000

    fix navbar, add portfolio page, add photo and name, work on nav anchoring

--- public/js/main.js
+++ public/js/main.js
@@ -5,40 +5,18 @@ window.addEventListener('DOMContentLoaded', () => {
     sidebarIcons.forEach((sidebarIcon, index) => {
         sidebarIcon.style.animation = `sidebarStart 0.2s ${index * 0.1}s 2 alternate`;
     });
-    navItems.forEach((navItem, index) => {
-        navItem.style.animation = `navStart 0.2s ${index * 0.1}s 2 alternate`;
-    });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    let navbar = document.getElementById("navbar");
-    let navbarOffset = 0;
+let prevScrollPos = window.pageYOffset;
 
-    do {
-        navbarOffset += navbar.offsetTop;
-        navbar = navbar.offsetParent;
-    } while (navbar);
-    
-    navbarOffset = navbarOffset < 0 ? 0 : navbarOffset;
-    const stickyName = document.querySelector(".name-nav-sticky");
-  
-    function handleScroll() {
-      if (window.pageYOffset >= navbarOffset) {
-        navbar.classList.add("sticky");
-        console.log("sticky class added");
-        stickyName.style.animation = "grow 0.2s 1 forwards";
-        navItems.forEach((navItem) => {
-            navItem.classList.add("sticky-hover");
-        });
-      } else {
-        console.log("sticky class removed");
-        navbar.classList.remove("sticky");
-        stickyName.style.animation = "shrink 0.2s 1 forwards";
-        navItems.forEach((navItem) => {
-            navItem.classList.remove("sticky-hover");
-        });
-      }
-    }
-  
-    window.addEventListener("scroll", handleScroll);
-});
+window.onscroll = () => {
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos < currentScrollPos) {
+    document.getElementById("navbar").style.animation = "navScrollUp 0.5s 1 forwards";
+  } else {
+    document.getElementById("navbar").style.animation = "navScrollDown 0.5s 1 forwards"; /* Adjust the value based on your navbar height */
+  }
+
+  prevScrollPos = currentScrollPos;
+};
